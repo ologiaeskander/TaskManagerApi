5e5678c76ab5c75759a994823b08c5e35edbfe7d
ologiaeskander <ologiaeskander@gmail.com>
2025-10-21 17:19:43 +0300

    Created the base models

---
+++ FirstAPI/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstAPI.Models
+{
+    public enum Status { ToDo, InProgress, Done }
+    public enum Priority { Low, Medium, High }
+
+    public class Task
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public Status Status { get; set; }
+        public Priority Priority { get; set; }
+        public int AssignedToUserId { get; set; }
+        public int? ProjectId { get; set; } //nullable in case of rogue/general tasks
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DueDate { get; set; } 
+        public int CreatedBy { get; set; }
+    }
+}
---
+++ FirstAPI/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstAPI.Models
+{
+    public enum Role { Manager, Employee}
+    public enum Department { Technical, Marketing, HR}
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [Required]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public Role Role {get; set;} 
+        public Department Department { get; set;}
+    }
+}
