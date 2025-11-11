49e6b22efa5e50eb1ce237a8bc61c70cf4fec281
ologiaeskander <ologiaeskander@gmail.com>
2025-11-11 13:45:44 +0200

    Added swagger

--- TaskManagerApi/Program.cs
+++ TaskManagerApi/Program.cs
@@ -2,12 +2,15 @@ using Microsoft.EntityFrameworkCore;
 using TaskManagerApi.Data;
 using Microsoft.AspNetCore.Identity;
 using YourProjectName.Models;
+using Swashbuckle.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -16,6 +19,19 @@ builder.Services.AddDbContext<TaskManagerContext>(options => options.UseSqlServe
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<TaskManagerContext>();
 
 var app = builder.Build();
+
+// And these middlewares (order matters!)
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();           // ? Enables Swagger JSON generation
+    app.UseSwaggerUI();         // ? Enables the Swagger UI
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
