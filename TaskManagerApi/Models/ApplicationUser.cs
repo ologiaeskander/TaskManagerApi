7875b256f3671a5ebbd8375e09ddb9694d6223b3
ologiaeskander <ologiaeskander@gmail.com>
2025-11-11 11:14:19 +0200

    Added new fields to register but maintained the username as is (still the same as email)

--- TaskManagerApi/Models/ApplicationUser.cs
+++ TaskManagerApi/Models/ApplicationUser.cs
@@ -4,6 +4,9 @@ namespace YourProjectName.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        //add custom properties here later
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int UsernameChangeLimit { get; set; } = 10;
+        public byte[] ProfilePicture { get; set; }
     }
 }
