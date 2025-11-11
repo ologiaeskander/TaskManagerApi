c7ef33213460d8b28f6997eae84eeb2ccd11ef5d
ologiaeskander <ologiaeskander@gmail.com>
2025-11-11 10:11:07 +0200

    Changed ugly Identity table names

--- TaskManagerApi/Data/TaskManagerContext.cs
+++ TaskManagerApi/Data/TaskManagerContext.cs
@@ -9,12 +9,41 @@ namespace TaskManagerApi.Data
     public class TaskManagerContext :IdentityDbContext<ApplicationUser>
     {
         public TaskManagerContext(DbContextOptions<TaskManagerContext> options) : base(options) { }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
-
         public DbSet<Job> Jobs { get; set; }
         public DbSet<Project> Projects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("Identity");
+            builder.Entity<IdentityUser>(entity =>
+            {
+                entity.ToTable(name: "User");
+            });
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable(name: "Role");
+            });
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable("UserRoles");
+            });
+            builder.Entity<IdentityUserClaim<string>>(entity =>
+            {
+                entity.ToTable("UserClaims");
+            });
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable("UserLogins");
+            });
+            builder.Entity<IdentityRoleClaim<string>>(entity =>
+            {
+                entity.ToTable("RoleClaims");
+            });
+            builder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.ToTable("UserTokens");
+            });
+        }
     }
 }
